9283820f9361df4d9a9d6fd556136d3319284699
Rr4d <162566259+Rr4d@users.noreply.github.com>
2025-09-01 16:43:51 +0800

    Add files via upload

---
+++ main_page.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<head>
+    <title>Kanaan Christian High School Banjarmasin's Library</title>
+</head>
+
+<body>
+    <div style="
+    display:flex; 
+    justify-content: center;
+    align-items:center;
+    ">
+        <h1>Welcome to the Library of Kanaan Christian High School Banjarmasin</h1>
+        
+    </div>
+    <a href="books/index.php">View Book Collection</a>
+    <a href="user_action/view_user.php">Manage User</a>
+</body>
+
+</html>
